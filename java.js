70775205fefbdfd859ebc5cee9144c42349c938f
sandhyarameshiyer <sandhya.ramesh0606@gmail.com>
2020-05-19 13:36:02 +0530

    first commit

---
+++ java.js
@@ -0,0 +1,9 @@
+document.getElementById('sign').addEventListener('click',
+function() {
+document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+document.querySelector('.close').addEventListener('clcik',
+function() {
+document.querySelector('.bg-modal').style.display = 'none';
+});
